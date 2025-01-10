dc21ea61559fe9a444b724b76fd55f8b415e3ee4
Hatosu <tylerjacobcox@tutamail.com>
2025-01-10 00:15:02 -0800

    fix install script

--- pkgs/install/default.nix
+++ pkgs/install/default.nix
@@ -6,18 +6,18 @@
   sudo sgdisk -Z /dev/nvme0n1
 
   # fetch files
-  sudo nix --experimental-features "nix-command flakes" run nixpkgs#git -- clone https://github.com/hatosu/config /tmp/config
+  sudo nix --experimental-features "nix-command flakes" run nixpkgs#git -- clone https://github.com/hatosu/nix /tmp/nix
 
   # format disk
-  sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/config/profile/temporary/disk.nix --arg device '"/dev/nvme0n1"'
+  sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/nix/profile/temporary/disk.nix --arg device '"/dev/nvme0n1"'
 
   # generate config
   sudo nixos-generate-config --no-filesystems --root /mnt
 
   # alter & move files
-  sudo rm -f /tmp/config/flake.lock
+  sudo rm -f /tmp/nix/flake.lock
   sudo rm -f /mnt/etc/nixos/configuration.nix
-  sudo cp -rf /tmp/config/* /mnt/etc/nixos
+  sudo cp -rf /tmp/nix/* /mnt/etc/nixos
   sudo mv -f /mnt/etc/nixos/hardware-configuration.nix /mnt/etc/nixos/profile/temporary/hardware.nix
 
   # backup
