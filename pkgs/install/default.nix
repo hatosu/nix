4a756467e1c373fef5e3f3fcc63cdbf8a0a012aa
System administrator <root@nixos.>
2025-01-25 04:11:05 -0800

    update install script

--- pkgs/install/default.nix
+++ pkgs/install/default.nix
@@ -1,30 +1,21 @@
- { pkgs ? import <nixpkgs> {} }: pkgs.writeShellScriptBin "install" ''
-
-  #!/usr/bin/env bash
-
-  # wipe drive
-  sudo sgdisk -Z /dev/nvme0n1
-
-  # fetch files
-  sudo nix --experimental-features "nix-command flakes" run nixpkgs#git -- clone https://github.com/hatosu/nix /tmp/nix
-
-  # format disk
-  sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/nix/profile/temporary/disk.nix --arg device '"/dev/nvme0n1"'
-
-  # generate config
+let pkgs = import <nixpkgs> {};
+  SHELL = "#!/usr/bin/env bash";
+  DIR = "nix";
+  GIT = "https://github.com/hatosu/${DIR}";
+in pkgs.writeShellScriptBin "install" ''
+  ${SHELL}
+  sudo nix --experimental-features "nix-command flakes" run nixpkgs#git -- clone ${GIT} /tmp/${DIR}
+  lsblk && printf ""
+  printf "drive to use: " >&2
+  read -r _DRIVE
+  sudo sgdisk -Z "/dev/$_DRIVE"
+  sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/${DIR}/profile/temporary/disk.nix --arg device '"/dev/$_DRIVE"'
   sudo nixos-generate-config --no-filesystems --root /mnt
-
-  # alter & move files
-  sudo rm -f /tmp/nix/flake.lock
+  sudo rm -f /tmp/${DIR}/flake.lock
   sudo rm -f /mnt/etc/nixos/configuration.nix
-  sudo cp -rf /tmp/nix/* /mnt/etc/nixos
+  sudo cp -rf /tmp/${DIR}/* /mnt/etc/nixos
   sudo mv -f /mnt/etc/nixos/hardware-configuration.nix /mnt/etc/nixos/profile/temporary/hardware.nix
-
-  # backup
   sudo mkdir /persist
   sudo cp -rf /mnt/etc/nixos /persist
-
-  # install nixos
   sudo nixos-install --root /mnt --flake /mnt/etc/nixos#temporary
-
 ''
